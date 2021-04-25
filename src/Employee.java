110af6e2ed6374b140bb1fbe562d5108e207c639
Naveen <naveengali80@gmail.com>
2021-04-25 08:54:32 +0530

    Updated and Committed the Project.

--- src/Employee.java
+++ src/Employee.java
@@ -8,19 +8,20 @@ class Employee {
     String name;
     String address;
 
-    Employee(int i,String n,String a){
+    Employee(int i, String n, String a) {
         id = i;
         name = n;
         address = a;
     }
 
-    void Display(){
-        System.out.println("Id :- "+id);
-        System.out.println("Name :- "+name);
-        System.out.println("Address :- "+address);
+    void Display() {
+        System.out.println("Id :- " + id);
+        System.out.println("Name :- " + name);
+        System.out.println("Address :- " + address);
     }
 }
-class EmpList{
+
+class EmpList {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -30,7 +31,7 @@ class EmpList{
 
         ArrayList<Employee> arl = new ArrayList<>();
 
-        for (int i=0;i<5;i++){
+        for (int i = 0; i < 5; i++) {
 
             System.out.println("Enter id :- ");
             id = Integer.parseInt(br.readLine());
@@ -41,8 +42,24 @@ class EmpList{
             System.out.println("Enter Address :- ");
             address = br.readLine();
 
-            Employee emp = new Employee(id,name,address);
+            Employee emp = new Employee(id, name, address);
             arl.add(emp);
+        }
+        System.out.println("Enter id to Search :- ");
+        id = Integer.parseInt(br.readLine());
+
+        boolean found = false;
+
+        for (int i=0;i<arl.size();i++){
+            Employee obj = arl.get(i);
+
+            if (id == obj.id){
+                obj.Display();
+                found = true;
+            }
+        }
+        if (!found){
+            System.out.println("Employee Not Found!");
         }
     }
 }
